e83d695084831c8b1b323fc586e6147668619f46
AdithyaShenal <washenal55@gmail.com>
2024-07-12 22:25:50 +0530

    bug fixed

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -3,13 +3,9 @@ import NavBar from "./NavBar";
 import ItemGrid from "./ItemGrid";
 import { useState } from "react";
 import { ItemProps } from "./ItemCard";
-import ItemFilter from "./ItemFilter";
-import itemsArray from "../services/items";
 
 const HomePage = () => {
   const [cartItems, setCartItems] = useState<ItemProps[]>([]);
-
-  const [items, setItems] = useState<ItemProps[]>(itemsArray);
 
   const onAddItem = (data: ItemProps) => {
     setCartItems([...cartItems, data]);
